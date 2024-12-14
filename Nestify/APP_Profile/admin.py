acdc3e744fa6c51e4aaa3f17273b8da7bdb25787
Vysockis <my@email.com>
2024-12-14 03:01:05 +0200

    Initial check-in

---
+++ Nestify/APP_Profile/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from . import models
+from django.contrib.auth.admin import UserAdmin
+
+# Register your models here.
+class ProfileUserAdmin(UserAdmin):
+    # Add your custom fields to the fieldsets or add_fieldsets attribute
+    fieldsets = UserAdmin.fieldsets + (
+       ('Custom Fields', {'fields': (
+           'birth',)}),
+    )
+
+admin.site.register(models.CustomUser, ProfileUserAdmin)
