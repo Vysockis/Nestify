89a4a2c9a7030f40e0560c4699510ea9b2c3d6d7
Vysockis <my@email.com>
2025-04-28 02:52:24 +0300

    Postgresql implemented

--- Nestify/Nestify/settings.py
+++ Nestify/Nestify/settings.py
@@ -100,8 +100,12 @@ WSGI_APPLICATION = 'Nestify.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config['database']['name'],
+        'USER': config['database']['user'],
+        'PASSWORD': config['database']['password'],
+        'HOST': config['database']['host'],
+        'PORT': config['database']['port'],
     }
 }
 
